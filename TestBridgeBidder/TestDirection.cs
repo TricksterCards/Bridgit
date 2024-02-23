792856a70ef1c4ecd47fef26560ab581ec418877
Ralph Lipe <ralph@lipe.ws>
2024-02-22 23:20:12 -0800

    Create TestDirection.cs
    Unit tests

---
+++ TestBridgeBidder/TestDirection.cs
@@ -0,0 +1,36 @@
+using BridgeBidding;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBridgeBidder
+{
+    [TestClass]
+    public class TestDirection
+    {
+        [TestMethod]
+        public void TestLeftHandOpponent()
+        {
+            Assert.AreEqual(Direction.E, Direction.N.LeftHandOpponent());
+            Assert.AreEqual(Direction.S, Direction.E.LeftHandOpponent());
+            Assert.AreEqual(Direction.W, Direction.S.LeftHandOpponent());
+            Assert.AreEqual(Direction.N, Direction.W.LeftHandOpponent());
+        }
+
+        [TestMethod]
+        public void TestPartner()
+        {
+            Assert.AreEqual(Direction.N, Direction.S.Partner());
+            Assert.AreEqual(Direction.E, Direction.W.Partner());
+            Assert.AreEqual(Direction.S, Direction.N.Partner());
+            Assert.AreEqual(Direction.W, Direction.E.Partner());
+        }
+
+        [TestMethod]
+        public void TestRightHandOpponent()
+        {
+            Assert.AreEqual(Direction.W, Direction.N.RightHandOpponent());
+            Assert.AreEqual(Direction.N, Direction.E.RightHandOpponent());
+            Assert.AreEqual(Direction.E, Direction.S.RightHandOpponent());
+            Assert.AreEqual(Direction.S, Direction.W.RightHandOpponent());
+        }
+    }
+}
